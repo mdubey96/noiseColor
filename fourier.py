e31dff60e398aa46a615c2d9327adf0e3170271b
Titanslayer1104 <delarivajuan@outlook.com>
2020-07-21 14:31:01 -0600

    Modified the code to be more clean... Added several png plots to the spectrogram file... Added the different types of noise such as white, blue, and pink...

--- fourier.py
+++ fourier.py
@@ -3,11 +3,11 @@ import librosa, librosa.display
 import numpy as np, scipy, matplotlib.pyplot as plt, IPython.display as ipd
 
 
-#Testing the librosa library...
-(sig, rate) = librosa.load('data/soundFiles/audiocheck.net_whitenoise.wav', sr=None)
-print(sig.size/rate)
+#Loading a sound file...
+(sig, rate) = librosa.load('data/soundFiles/waterSound1_1.wav', sr=None)
 
-#Creating Spectrogram of specified sound file...
+
+#Creating a Spectrogram of specified sound file...
 def spectrogram(sig, rate):
         f = np.linspace(0, rate/2, 4096)
         X = scipy.fft.fft(sig[75000:125000])
@@ -17,38 +17,28 @@ def spectrogram(sig, rate):
         plt.xlabel('Frequency (Hz)')
         plt.show()
 
-spectrogram(sig, rate)
+#spectrogram(sig, rate)
 
-#Testing the plot library...
-'''
-#Simple Plotting
-#plt.plot(sig)
-#plt.ylabel('Amplitude')
-#plt.xlabel('Time')
-#plt.show()
-'''
+#Creating a spectrogram set to log-log scale...
+def spec_log(sig, rate):
+        f = np.linspace(0, rate/2, 4096)
+        X = scipy.fft.fft(sig[75000:125000])
+        X_mag = np.absolute(X)
+        
+        dB = 20*np.log10(X_mag/np.amax(X_mag))
+        
+        plt.figure(figsize=(14,5))
+        plt.plot(f[50:4000], dB[50:4000])
+        plt.xscale('log')
+        plt.xlabel('Frequency (Hz)')
+        #plt.show()
 
-#Testing if code works...
-'''
-T = 2.0 # seconds
-f0 = 1047.0
-sr = 22050
-f = np.linspace(0, sr, 4096)
-t = np.linspace(0, T, int(T*sr), endpoint=False) # time variable
-x = 0.1*np.sin(2*numpy.pi*f0*t)
-X = scipy.fft(x[10000:14096])
-X_mag = np.absolute(X)
-plt.figure(figsize=(14, 5))
-plt.plot(f[:2000], X_mag[:2000]) # magnitude spectrum
-plt.xlabel('Frequency (Hz)')
-plt.show()
-'''
+spec_log(sig, rate)
 
 #Outputing the spectrograms as png's...
 '''
-for i in range(3): 
-	for p in range(5):
-		(sig, rate) = librosa.load('data/waterSound'+str(i+1)+'_'+str(p+1)+'.wav', sr=None)
-		spectrogram(sig, rate)
-		plt.savefig('spectrogram'+str(i+1)+'_'+str(p+1)+'.png')
+for i in range(3):
+    for p in range(5):
+        #Load the file
+        #plt.savefig('File name')
 '''
