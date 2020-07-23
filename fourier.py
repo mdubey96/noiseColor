1c52166a97ed67afeab6b033d8c254e4d625e839
Titanslayer1104 <delarivajuan@outlook.com>
2020-07-23 14:44:46 -0600

    The code will now plot a line along with the spectrogram to better identify the noise and frequencies.

--- fourier.py
+++ fourier.py
@@ -1,15 +1,16 @@
 #Import all the required libraries...
 import librosa, librosa.display
 import numpy as np, scipy, matplotlib.pyplot as plt, IPython.display as ipd
-
+from scipy.optimize import curve_fit
 
 #Loading a sound file...
-(sig, rate) = librosa.load('data/soundFiles/lakeshoreWaves.wav', sr=None)
+(sig, rate) = librosa.load('data/soundFiles/waterSound1_1.wav', sr=None)
 
 
 #Creating a Spectrogram of specified sound file...
+'''
 def spectrogram(sig, rate):
-        f = np.linspace(0, rate/2, 4096)
+        n = np.linspace(0, rate/2, 4096)
         X = scipy.fft.fft(sig[75000:125000])
         X_mag = np.absolute(X)
         plt.figure(figsize=(14,5))
@@ -18,17 +19,24 @@ def spectrogram(sig, rate):
         plt.show()
 
 spectrogram(sig, rate)
+'''
 
 #Creating a spectrogram set to log-log scale...
 def spec_log(sig, rate):
-        f = np.linspace(0, rate/2, 4096)
+        n = np.linspace(0, rate/2, 4096)
         X = scipy.fft.fft(sig[75000:125000])
-        X_mag = np.absolute(X)
-        
+        X_mag = np.absolute(X)        
         dB = 20*np.log10(X_mag/np.amax(X_mag))
+
+        def f(x, A, B):
+                return A*x + B
         
+        popt, pcov = curve_fit(f, n[:4000], dB[:4000])
+
         plt.figure(figsize=(14,5))
-        plt.plot(f[50:4000], dB[50:4000])
+        
+        plt.plot(n[50:4000], dB[50:4000])
+        plt.plot(n[50:4000], f(n, *popt)[50:4000])
         plt.xscale('log')
         plt.xlabel('Frequency (Hz)')
         plt.show()
