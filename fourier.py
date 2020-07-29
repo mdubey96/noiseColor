2692db1c743dda9f7dd923930b9a054f21511c42
Titanslayer1104 <delarivajuan@outlook.com>
2020-07-29 13:37:00 -0600

    Adds a text file for slopes and intercepts of data; removed unnecessary directories; Modified code...

--- fourier.py
+++ fourier.py
@@ -3,12 +3,7 @@ import librosa, librosa.display
 import numpy as np, scipy, matplotlib.pyplot as plt, IPython.display as ipd, pandas as pd, glob
 from scipy.optimize import curve_fit
 
-
-#Loading a sound file...
-#(sig, rate) = librosa.load('data/soundFiles/whitenoise.wav', sr=None)
-
-
-#Creating a Spectrogram of specified sound file...
+#Creating a Spectrogram of specified sound file(simple)...
 '''
 def spectrogram(sig, rate):
         n = np.linspace(0, rate/2, 4096)
@@ -24,6 +19,9 @@ spectrogram(sig, rate)
 
 #Creating a spectrogram set to log-log scale...
 def spec_log(sig, rate):
+        global popt
+        global pcov
+        
         n = np.linspace(0, rate/2, 4096)
         X = scipy.fft.fft(sig[75000:125000])
         X_mag = np.absolute(X)        
@@ -41,12 +39,13 @@ def spec_log(sig, rate):
         plt.xscale('log')
         plt.xlabel('Frequency (Hz)')
         #plt.show()
+        
 
 #spec_log(sig, rate)
 
 
 #Outputing the spectrograms as png's...
-#all_files = glob.glob('/noiseColor/data/soundFiles/*')
+'''
 for file in glob.glob('/home/titanslayer/2020internproj/noiseColor/data/soundFiles/*'):
         file = file.replace('/home/titanslayer/2020internproj/noiseColor/', '')
         (sig, rate) = librosa.load(file, sr=None)
@@ -56,3 +55,21 @@ for file in glob.glob('/home/titanslayer/2020internproj/noiseColor/data/soundFil
         file = file.replace('.wav', '.png')
         
         plt.savefig('log_'+file)
+'''
+
+#Outputing the data & slopes to a text file...
+'''
+file = open('data_comparison.txt', 'w')
+
+for files in glob.glob('/home/titanslayer/2020internproj/noiseColor/data/soundFiles/*'):
+        files = files.replace('/home/titanslayer/2020internproj/noiseColor/', '')
+        (sig, rate) = librosa.load(files, sr=None)
+        spec_log(sig, rate)
+        def define_noise():
+                if abs(pop
+        files = files.replace('data/soundFiles/', '')
+
+        file.write(files + ' ; Slope=' + str(popt[0]) + ' ; Intercept=' + str(popt[1]) + ' ; Noise='+ '\n\n')
+
+file.close()
+'''
